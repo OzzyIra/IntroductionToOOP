b607a0f2600621f5ceb9b596a2ea39ff2b887636
OzzyIra <163173290+OzzyIra@users.noreply.github.com>
2024-06-17 21:07:11 +0300

    Metods out of class

--- String/main.cpp
+++ String/main.cpp
@@ -8,70 +8,21 @@ class String
     int size; //размер строки в байтах
     char* str; // адрес строки в динамической памяти
 public:
-    const char* get_str()const
-    {
-        return str;
-    }
-    char* get_str()
-    {
-        return str;
-    }
-    int get_size()const
-    {
-        return size;
-    }
-    void set_str(char* str)
-    {
-        this->str = str;
-    }
-    void set_size(int size)
-    {
-        this->size;
-    }
+    const char* get_str()const;
+    char* get_str();
+    int get_size()const;
+    void set_str(char* str);
+    void set_size(int size);
 
     //      constructors:
-    explicit String(int size = 80) :size(size), str(new char[size]{})
-    {
-        //this->size = size;
-        //this->str = new char[size + 1] {};
-        cout << "DefaultConctructor:\t" << this << endl;
-    }
-    String(const char* str):String(strlen(str) + 1)
-    {
-       // this->size = strlen(str)+1;
-       // this->str = new char[size]{};
-        for (int i = 0; i < size; i++)
-        {
-            this->str[i] = str[i];
-        }
-        cout << "Constructer1\t\t" << this << endl;
-    }
+    explicit String(int size = 80);
+    String(const char* str);
   
-    String(const String& other):String(other.str)
-    {
-        //this->size = other.size;
-        //this->str = new char[other.size + 1]{};
-        // for (int i = 0; i < size; i++)this->str[i] = other.str[i];
-        cout << "CopyConstructor" << this << endl;
-    }
+    String(const String& other);
 
-    ~String()
-    {
-        delete[]this->str;
-        cout << "destructor:\t\t" << this << endl;
-    }
+    ~String();
  
-    String& operator=(const String& other)
-    {
-        if (this == &other)return *this;
-        delete[]str;
-        this->size = other.size;
-        this->str = new char[size + 1]{};
-        for (int i = 0; i < size; i++)
-            this->str[i] = other.str[i];
-        cout << "CopyAssignment:\t\t" << this << endl;
-        return *this;
-    }
+    String& operator=(const String& other);
     //String operator+(const String& other)
     //{
     //    String str_new;
@@ -94,15 +45,105 @@ public:
     //    return str_new;
     //}
     //      Methods
-    void print()const
-    {
-        cout << "Obj:\t" << this << "\t";
-        cout << "Size:\t" << size << endl;
-        cout << "Addr:\t" << &str << "\t";
-        cout << "Str:\t" << str << endl;
-    }
+    void print()const;
 };
+const char* String:: get_str()const
+{
+    return str;
+}
+char* String::get_str()
+{
+    return str;
+}
+int String:: get_size()const
+{
+    return size;
+}
+void String::set_str(char* str)
+{
+    this->str = str;
+}
+void String::set_size(int size)
+{
+    this->size;
+}
 
+//      constructors:
+String:: String(int size ) :size(size), str(new char[size] {})
+{
+    //this->size = size;
+    //this->str = new char[size + 1] {};
+    cout << "DefaultConctructor:\t" << this << endl;
+}
+String::String(const char* str) :String(strlen(str) + 1)
+{
+    // this->size = strlen(str)+1;
+    // this->str = new char[size]{};
+    for (int i = 0; i < size; i++)
+    {
+        this->str[i] = str[i];
+    }
+    cout << "Constructer1\t\t" << this << endl;
+}
+
+String::String(const String& other) :String(other.str)
+{
+    //this->size = other.size;
+    //this->str = new char[other.size + 1]{};
+    // for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+    cout << "CopyConstructor" << this << endl;
+}
+
+String::~String()
+{
+    delete[]this->str;
+    cout << "destructor:\t\t" << this << endl;
+}
+
+String& String::operator=(const String& other)
+{
+    if (this == &other)return *this;
+    delete[]str;
+    this->size = other.size;
+    this->str = new char[size + 1]{};
+    for (int i = 0; i < size; i++)
+        this->str[i] = other.str[i];
+    cout << "CopyAssignment:\t\t" << this << endl;
+    return *this;
+}
+//String operator+(const String& other)
+//{
+//    String str_new;
+//    int str1Size = this->get_size();
+//    int str2Size = other.get_size();
+
+//    int newSize = str1Size + str2Size;
+//    str_new = new char[newSize + 2]{};
+//    //  копируем
+//    int i;
+//    for ( i = 0; i < str1Size + 1; i++)
+//    {
+//        str_new.str[i] = this->str[i];
+//    }
+//    str_new.str[i] = ' ';
+//    for (int j = 0; j < str2Size; j++)
+//    {
+//        str_new.str[j] = other.str[j];
+//    }
+//    return str_new;
+//}
+//      Methods
+void String::print()const
+{
+    cout << "Obj:\t" << this << "\t";
+    cout << "Size:\t" << size << endl;
+    cout << "Addr:\t" << &str << "\t";
+    cout << "Str:\t" << str << endl;
+}
+std::ostream& operator<<(std::ostream& os, const String obj)
+{
+    return os<<obj.
+}
  String operator+(const String& left, const String& right)
 {
     String buffer(left.get_size() + right.get_size() - 1 );
